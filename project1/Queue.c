e2fdc1d9337b976faef22614809666baadd2d2db
tsitsg <tsitsg@ubuntu.ubuntu-domain>
2017-03-07 23:39:57 -0500

    Can insert to queue at any position

--- project1/Queue.c
+++ project1/Queue.c
@@ -21,6 +21,28 @@ void insert(myQueue* q, processInfo process) {
 	q->itemCount += 1;
 }
 
+void insertAt(myQueue* q, processInfo process, int position) {
+	if (q->itemCount >= q->size) { /* Resize array by doubling size */
+		q->size += 1;
+		q->queue = realloc(q->queue, q->size*sizeof(processInfo));
+	}
+
+	int i = 0;
+	int j = 0;
+	for (i; i < q->size; i++) {
+		if (i < position) {
+			continue;
+		} else if (i == position) {
+			for (j = q->size-1; j > position; j--) {
+				q->queue[j] = q->queue[j-1]; 
+			}
+			q->queue[position] = process;
+			q->itemCount += 1;
+			break;
+		}
+	}
+}
+
 char pop(myQueue* q) {
 	printf("The size before popping is: %d\n", q->size);
 
